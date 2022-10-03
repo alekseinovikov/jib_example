502c5a21fa953f35a51ef2c2f1e77e71f8f05fff
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2022-10-03 20:31:31 +0300

    Initial commit

---
+++ src/test/java/me/freedom4live/jib_example/JibExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.freedom4live.jib_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
